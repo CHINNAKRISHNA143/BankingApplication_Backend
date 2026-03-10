105cc4445106c07cbdcc815096a676a732659fdf
CHINNAKRISHNA143 <chinnikrishnamekala1@gmail.com>
2026-03-10 21:07:16 +0530

    SendEmail Method Impl

--- src/main/java/com/bank/util/EmailUtil.java
+++ src/main/java/com/bank/util/EmailUtil.java
@@ -1,10 +1,46 @@
 package com.bank.util;
 
+import java.util.Properties;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
 public class EmailUtil {
 	
-	public static void sendEmail(String email,String subject,String msg){
+	private static final String FROM_MAIL = "harikrishnahh403@gmail.com";
+	private static final String APP_PASSWORD = "rgwb fphd ucks xlab";
+	
+	public static void sendEmail(String email,String subject,String body){
 		
-		System.out.println("Hello");
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		
+		Session session = Session.getInstance(props, new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(FROM_MAIL,APP_PASSWORD);
+			}
+		});
+		
+		
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(FROM_MAIL));
+			//message.setRecipients(, null);
+			
+			
+			
+			
+		} catch (MessagingException e) {
+			 
+			e.printStackTrace();
+		}
 	}
 
 }
